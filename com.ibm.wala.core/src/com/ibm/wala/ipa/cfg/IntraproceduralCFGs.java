2cf9538fbee943ef65832e7b4c2a6b592dd13be3
Heng Li <henglicad@gmail.com>
2016-07-04 09:37:38 -0400

    create a class to handle all the intraprocedural CFGs

---
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/cfg/IntraproceduralCFGs.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.wala.ipa.cfg;
+
+import com.ibm.wala.ipa.cha.ClassHierarchy;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.classLoader.IClass;
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.types.ClassLoaderReference;
+import java.util.jar.JarFile;
+import java.io.IOException;
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+
+public class IntraproceduralCFGs {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+  
+  public void getAllMethods(AnalysisScope  scope) throws ClassHierarchyException {
+    IClassHierarchy cha = ClassHierarchy.make(scope);
+    for (IClass cl : cha) {
+      if (cl.getClassLoader().getReference().equals(ClassLoaderReference.Application)) {
+        for (IMethod m : cl.getAllMethods()) {
+          System.out.println(m.getSignature());
+        }
+      }
+    }
+  }
+
+}
